4065eefee440908ef38236ebd0f1aad435969e34
Alfonso-04 <66376481+Alfonso-04@users.noreply.github.com>
2023-10-18 20:50:50 -0500

    Repo
    .

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ProductTypesModule } from './product_types/product_types.module';
+import { MovementsModule } from './movements/movements.module';
+import { MovementTypeModule } from './movement_type/movement_type.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductType } from './product_types/entities/product_type.entity';
+import { Product } from './products/entities/product.entity';
+import { Movement } from './movements/entities/movement.entity';
+import { MovementType } from './movement_type/entities/movement_type.entity';
+
+@Module({
+  imports: [ProductsModule, ProductTypesModule, MovementsModule, MovementTypeModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '123456',
+      database: 'stock_db',
+      entities: [
+        ProductType,
+        Product,
+        Movement,
+        MovementType
+      ],
+      synchronize: true,
+    }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {}
